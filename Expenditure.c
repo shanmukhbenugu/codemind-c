b60506d1f9dc562ea593cd2be539886c6312fa5b
shanmukhbenugu <119918047+shanmukhbenugu@users.noreply.github.com>
2022-12-27 10:49:08 +0530

    #include<stdio.h> int main(){     int a,b,x;     scanf("%d%d",&a,&b);     x=30*b;     if(a>=x) printf("YES");     else printf("NO"); }

--- Expenditure.c
+++ Expenditure.c
@@ -3,10 +3,6 @@ int main(){
     int a,b,x;
     scanf("%d%d",&a,&b);
     x=30*b;
-    if(a>=x){
-        printf("YES");
-    }
-    else {
-        printf("NO");
-    }
+    if(a>=x) printf("YES");
+    else printf("NO");
 }
