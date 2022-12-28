708d7eed5775475c0276e52521e7191fa8a827aa
shanmukhbenugu <119918047+shanmukhbenugu@users.noreply.github.com>
2022-12-28 10:52:53 +0530

    #include<stdio.h> int main(){     int a,b,i,c,d,sum=0;     scanf("%d%d%d%d",&a,&b,&c,&d);     for(i=a;i<=b;i++){         if(i%c==0&&i%d!=0){             sum=sum+i;         }
    }
      printf("%d",sum);  
    }

---
+++ Sum_2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a,b,i,c,d,sum=0;
+    scanf("%d%d%d%d",&a,&b,&c,&d);
+    for(i=a;i<=b;i++){
+        if(i%c==0&&i%d!=0){
+            sum=sum+i;
+        }
+        
+}
+  printf("%d",sum);  
+}
