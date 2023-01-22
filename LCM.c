decf0ac04c04471a65ad8d394da0be3350d9e117
shanmukhbenugu <119918047+shanmukhbenugu@users.noreply.github.com>
2023-01-22 09:53:32 +0530

    #include<stdio.h> int main(){     int a,b,lcm;     scanf("%d%d",&a,&b);     lcm =a>b?a:b;     while(1){         if(lcm%a==0&&lcm%b==0){             printf("%d",lcm);             break;     }     lcm++; }}

--- LCM.c
+++ LCM.c
@@ -2,13 +2,11 @@
 int main(){
     int a,b,lcm;
     scanf("%d%d",&a,&b);
-    lcm=a>b?a:b;
-    while(1)
-    {
+    lcm =a>b?a:b;
+    while(1){
         if(lcm%a==0&&lcm%b==0){
             printf("%d",lcm);
             break;
-        }
-lcm++;
-}
-}
+    }
+    lcm++;
+}}
